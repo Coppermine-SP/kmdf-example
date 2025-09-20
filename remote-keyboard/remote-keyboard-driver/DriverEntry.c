78040d7b1c87ea54433aec3c8a78f2f50274a4ec
copperminesp <son5121502@gmail.com>
2025-09-20 20:15:50 +0900

    init: kmdf project

---
+++ remote-keyboard/remote-keyboard-driver/DriverEntry.c
@@ -0,0 +1,15 @@
+/*
+*	remote-keyboard-drvier - DriverEntry.c
+*	원격 가상 키보드 장치를 구현하는 예제입니다.
+*
+*	Copyright (C) 2025-2026 Coppermine-SP
+*/
+
+#include "remote-keyboard-driver.h"
+
+NTSTATUS DriverEntry(
+	IN PDRIVER_OBJECT DriverObject,
+	IN PUNICODE_STRING RegistryPath
+) {
+
+}
---
+++ remote-keyboard/remote-keyboard-driver/remote-keyboard-driver.h
@@ -0,0 +1,19 @@
+/*
+*	remote-keyboard-drvier - remote-keyboard-driver.h
+*	원격 가상 키보드 장치를 구현하는 예제입니다.
+* 
+*	Copyright (C) 2025-2026 Coppermine-SP 
+*/
+
+#pragma once
+
+#pragma region Includes
+#include <ntddk.h>
+#include "wsk.h"
+#pragma endregion
+
+#pragma region Defines
+#pragma endregion
+
+#pragma region Functions
+#pragma endregion
